429ae38696b849f8aaf02db036bb59e98d21bea0
Victor Fugazzotto <victorfuga@gmail.com>
2019-01-06 10:58:13 -0500

    Starting in C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PolyrealmBot
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await Task.Delay(-1);
+        }
+    }
+}
